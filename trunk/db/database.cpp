76d32d3391c6703762a181d4434d23676cba35c8
marro.somon <marro.somon@c9fdfb32-f78d-11dd-ac65-f74f249cfaac>
2009-03-07 22:31:48 +0000

    Added a new function get_server_ip_list(). It will return all server ips saved in database.
    I also changed the class name from db to database because a variable name was called db.
    
    created database() the defaulst constructor.

--- trunk/db/database.cpp
+++ trunk/db/database.cpp
@@ -1,6 +1,14 @@
 #include "database.h"
  
-void db::db_init()
+database::database()
+{
+  rc = 0;
+  dbname="cardb";
+  iNumProc = 0, iChildiStatus = 0, iStatus = 0, iDeadId = 0, iExitFlag = 0;
+  dbnameCstr = const_cast<char *>(dbname.c_str()); //dbname has to be in const char*
+  rc = sqlite3_open(dbnameCstr, &db);
+}
+void database::db_init()
 {
   rc = 0;
   dbname ="cardb";
@@ -19,33 +27,33 @@ void db::db_init()
   iStatus = RunCommand("sqlite3 cardb < schema");
   if (!iStatus)
     iNumProc++;
-	
+        
   // Wait till the commands complete
   while (iNumProc && !iExitFlag)
     {
       iDeadId = waitpid(-1, &iChildiStatus, WNOHANG);
       if (iDeadId < 0)
-	{
-	  // Wait id error - exit the loop
-	  iExitFlag = 1;
-	}
+        {
+          // Wait id error - exit the loop
+          iExitFlag = 1;
+        }
       else if (iDeadId > 0)
-	{
-	  iNumProc--;
-	  // You can check the process exit iStatus here - its in the
-	  // iChildiStatus variable
-	}
+        {
+          iNumProc--;
+          // You can check the process exit iStatus here - its in the
+          // iChildiStatus variable
+        }
       else  // iDeadId == 0, no processes died
-	{
-	  sleep(3);	// give them time to die
-	}
+        {
+          sleep(3);     // give them time to die
+        }
       
     } //end while (iNumProc && !iExitFlag) 
   /* Jan Koci code portion ends here.*/
   
 }
 
-int db::RunCommand(const char *strCommand)
+int database::RunCommand(const char *strCommand)
 {
   /* Function by...
    * Author: Jan Koci 
@@ -53,18 +61,18 @@ int db::RunCommand(const char *strCommand)
    */
 
   iForkId = vfork();
-  if (iForkId == 0)	// This is the child 
+  if (iForkId == 0)     // This is the child 
     {
       iStatus = execl("/bin/sh","sh","-c", strCommand, (char*) NULL);
-      exit(iStatus);	// We must exit here, 
+      exit(iStatus);    // We must exit here, 
       // or we will have multiple
       // mainlines running...  
     }
-  else if (iForkId > 0)	// Parent, no error
+  else if (iForkId > 0) // Parent, no error
     {
       iStatus = 0;
     }
-  else	// Parent, with error (iForkId == -1)
+  else  // Parent, with error (iForkId == -1)
     {
       iStatus = -1;
     }
@@ -72,7 +80,7 @@ int db::RunCommand(const char *strCommand)
 
 }
 
-void db::insert_ip(string sql) //this insert to the listIP table
+void database::insert_ip(string sql) //this insert to the listIP table
 {
   /*
     int sqlite3_exec(
@@ -83,8 +91,9 @@ void db::insert_ip(string sql) //this insert to the listIP table
     char **errmsg                              / Error msg written here /
     );
   */
-  dbname ="cardb";
-  rc = sqlite3_open(dbnameCstr, &db);
+  //dbname ="cardb";
+ // rc = sqlite3_open(dbnameCstr, &db);
+
 
   string temp = "insert into listIP values(\"" + sql;
   temp = temp + "\");"; 
@@ -95,7 +104,7 @@ void db::insert_ip(string sql) //this insert to the listIP table
   prepare = sqlite3_exec(db, sqlCstr, NULL, NULL, NULL );
 }
 
-void db::insert_file(string file_sql, string ip_sql) //this insert to the listIP table
+void database::insert_file(string file_sql, string ip_sql) //this insert to the listIP table
 {
   /*
     int sqlite3_exec(
@@ -106,8 +115,8 @@ void db::insert_file(string file_sql, string ip_sql) //this insert to the listIP
     char **errmsg                              / Error msg written here /
     );
   */
-  dbname ="cardb";
-  rc = sqlite3_open(dbnameCstr, &db);
+  //dbname ="cardb";
+  //rc = sqlite3_open(dbnameCstr, &db);
 
   string temp = "insert into files values(\"" + file_sql + "\",\"" + ip_sql + "\");";
   const char* sqlCstr = const_cast<char *>(temp.c_str()); //has to be in const char*
@@ -117,7 +126,7 @@ void db::insert_file(string file_sql, string ip_sql) //this insert to the listIP
 
 }
 
-int db::search_file(string fileName)
+int database::search_file(string fileName)
 {
 
   /*
@@ -131,11 +140,11 @@ int db::search_file(string fileName)
     );
   */
   
-  rc = 0;
-  dbname ="cardb";
+  //rc = 0;
+  //dbname ="cardb";
   dbnameCstr = const_cast<char *>(dbname.c_str()); //dbname has to be in const char*
+  //rc = sqlite3_open(dbnameCstr, &db);
 
-  rc = sqlite3_open(dbnameCstr, &db);
 
   if( rc!=SQLITE_OK )
     {
@@ -143,23 +152,113 @@ int db::search_file(string fileName)
       sqlite3_free(zErrMsg);
     } 
 
-  string sql = "select clientip from files where names=\""+ fileName + "\";";
+  string sql = "select clientip from files where names='"+ fileName + "'";
   const char* sqlCstr = const_cast<char *>(sql.c_str()); //has to be in const char*
   int prepare;
-  prepare = sqlite3_get_table( db, sqlCstr, &queryResult, &pnRow, &pnColumn, errmsg); 
-  searchHelper(); 
+
+
+
+  prepare = sqlite3_get_table( db, sqlCstr, &queryResult, &pnRow, &pnColumn, &errmsg); 
+  sqlite3_free(errmsg);
+  int i;
+
+  for(i = 1; i <= pnRow; ++i)
+    {
+      buffer.push_back(queryResult[i]);
+    }
+
   int result = pnRow;
+  sqlite3_free_table(queryResult);
   return result; 
 }
 
+int database::get_server_ip_list()
+{
+  dbnameCstr = const_cast<char *>(dbname.c_str()); //dbname has to be in const char*
+  if( rc!=SQLITE_OK )
+    {
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+    } 
+
+  string sql = "SELECT * FROM serverIP;";
+  const char* sqlCstr = const_cast<char *>(sql.c_str()); //has to be in const char*
+  int prepare;
+
+  prepare = sqlite3_get_table( db, sqlCstr, &queryResult, &pnRow, &pnColumn, &errmsg); 
+  sqlite3_free(errmsg);
+  int i;
+
+  for(i = 1; i <= pnRow; ++i)
+    {
+      buffer.push_back(queryResult[i]);
+    }
+
+  int result = pnRow;
+  sqlite3_free_table(queryResult);
+
+  return result; 
+}
+
+/*
 void db::searchHelper ()
 { 
   int i;
-  string temp;
 
   for(i = 1; i <= pnRow; ++i)
     {
       buffer.push_back(queryResult[i]);
     }
   sqlite3_free_table(queryResult);
+}
+*/
+
+void database::deleteClientIP(string ip)
+{
+
+//DELETE FROM pies WHERE flavour='Lemon Meringue'/
+
+  string temp = "delete from listIP where clientip='" + ip;
+  temp = temp + "';"; 
+
+  const char* sqlCstr = const_cast<char *>(temp.c_str()); //has to be in const char*
+  int prepare;
+  prepare = sqlite3_exec(db, sqlCstr, NULL, NULL, NULL );
+}
+
+void database::insert_server_ip(string sql) //this insert to the listIP table
+{
+  /*
+    int sqlite3_exec(
+    sqlite3*,                                  / An open database /
+    const char *sql,                           / SQL to be evaluated /
+    int (*callback)(void*,int,char**,char**),  / Callback function /
+    void *,                                    / 1st argument to callback /
+    char **errmsg                              / Error msg written here /
+    );
+  */
+  //dbname ="cardb";
+ // rc = sqlite3_open(dbnameCstr, &db);
+
+
+  string temp = "insert into serverIP values(\"" + sql;
+  temp = temp + "\");"; 
+
+  const char* sqlCstr = const_cast<char *>(temp.c_str()); //has to be in const char*
+  int prepare;
+
+  prepare = sqlite3_exec(db, sqlCstr, NULL, NULL, NULL );
+}
+
+void database::deleteServerIP(string ip)
+{
+
+//DELETE FROM pies WHERE flavour='Lemon Meringue'/
+
+  string temp = "delete from serverIP where serverip='" + ip;
+  temp = temp + "';"; 
+
+  const char* sqlCstr = const_cast<char *>(temp.c_str()); //has to be in const char*
+  int prepare;
+  prepare = sqlite3_exec(db, sqlCstr, NULL, NULL, NULL );
 }
--- trunk/db/database.h
+++ trunk/db/database.h
@@ -13,12 +13,12 @@
 
 /*
 WARNING NOTE
-WARNING NOTE 
+WARNING NOTE
 WARNING NOTE
 
 you need to link the "-sqlite3" object file
 
--lsqlite3 
+-lsqlite3
 
 ex:
 g++ database.cpp -lsqlite3
@@ -27,61 +27,68 @@ g++ database.cpp -lsqlite3
 
 using namespace std;
 
-class db {
-public:
-      /* 
-       * Function: void db_init()
-       * Basically makes the CAR database
-       * if the CAR database exist it'll try to make it and complain
-       * it won't overwrite the existing database
-       */
-      void db_init();
-      
-      /* 
-       * Function: int RunCommand(const char *strCommand)
-       * Runs a series of command that create CAR database
-       */
-      int RunCommand(const char *strCommand);
-      
-      /* 
-       * Function: void insert_ip(string sql)
-       * Use this function to insert list of ips/clients/users
-       */ 
-      void insert_ip(string sql); // this insert to listIP table
-      
-      /* 
-       * Function: void insert_ip(string sql)
-       * Use this function to insert all files and what user are sharing them
-       */
-      void insert_file(string file_sql, string ip_sql); //this insert to the listIP table
-      
-      
-      /* 
-       * Function: int search_file(string fileName)
-       * Search function, returns number of results
-       * store the results/ip into a buffer or vector of strings                      
-       */            
-      int search_file(string fileName);
-      
-      void searchHelper ();
-      string getBuffer(int i){ return buffer[i]; };
-      void clearBuffer(){ buffer.clear(); };    
-
-private:
-      string query;
-      vector<string> buffer; 	// a list of queries
-      int iForkId, iStatus;
-      int iNumProc, iChildiStatus, iDeadId;
-      int iExitFlag,rc;
-      sqlite3 *db;
-      string dbname;
-      const char* dbnameCstr;
-      char **errmsg;
-      char *zErrMsg;
-      char **queryResult;    	/* Results of the query */
-      int pnRow;           	/* Number of result rows written here */
-      int pnColumn;        	/* Number of result columns written here */
+class database {
+ public:
+  database();
+  /*
+   * Function: void db_init()
+   * Basically makes the CAR database
+   * if the CAR database exist it'll try to make it and complain
+   * it won't overwrite the existing database
+   */
+  void db_init();
+  
+  /*
+   * Function: int RunCommand(const char *strCommand)
+   * Runs a series of command that create CAR database
+   */
+  int RunCommand(const char *strCommand);
+  
+  /*
+   * Function: void insert_ip(string sql)
+   * Use this function to insert list of ips/clients/users
+   */
+  void insert_ip(string sql); // this insert to listIP table
+  
+  /*
+   * Function: void insert_ip(string sql)
+   * Use this function to insert all files and what user are sharing them
+   */
+  void insert_file(string file_sql, string ip_sql); //this insert to the listIP table
+  
+  
+  /*
+   * Function: int search_file(string fileName)
+   * Search function, returns number of results
+   * store the results/ip into a buffer or vector of strings                      
+   */            
+  int search_file(string fileName);
+  
+  //void searchHelper ();
+  string getBuffer(int i){  return buffer[i]; };
+  void clearBuffer(){ buffer.clear(); };    
+  
+  void deleteClientIP(string ip);
+  
+  void deleteServerIP(string ip);
+  
+  void insert_server_ip(string sql);
+  
+  int get_server_ip_list();
+  
+ private:
+  string query;
+  vector<string> buffer;    // a list of queries
+  int iForkId, iStatus;
+  int iNumProc, iChildiStatus, iDeadId;
+  int iExitFlag,rc;
+  sqlite3 *db;
+  string dbname;
+  const char* dbnameCstr;
+  char *errmsg;
+  char *zErrMsg;
+  char **queryResult;       /* Results of the query */
+  int pnRow;                /* Number of result rows written here */
+  int pnColumn;             /* Number of result columns written here */
 };
-
-
 #endif
